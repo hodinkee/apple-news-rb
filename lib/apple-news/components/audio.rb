a4af6dd009aa413002f03030b12c92c0141e3fbc
Ryan LeFevre <meltingice8917@gmail.com>
2016-04-25 10:56:23 -0400

    Forgot to add property inflection to audio

--- lib/apple-news/components/audio.rb
+++ lib/apple-news/components/audio.rb
@@ -7,6 +7,9 @@ module AppleNews
         role "audio"
         required_property :url
         optional_properties :accessibility_caption, :caption, :explicit_content, :image_url
+
+        property_inflection :url, 'URL'
+        property_inflection :image_url, 'imageURL'
       end
     end
   end
