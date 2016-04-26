58daf3ed1d7af0198363828f7713bc640e94ca2c
Ryan LeFevre <meltingice8917@gmail.com>
2016-04-26 14:07:39 -0400

    Add metadata methods to document

--- lib/apple-news/document.rb
+++ lib/apple-news/document.rb
@@ -2,10 +2,11 @@ require 'stringio'
 
 module AppleNews
   class Document
-    attr_accessor :article
+    attr_accessor :article, :metadata
 
-    def initialize(article = nil)
+    def initialize(article = nil, metadata = {})
       @article = article
+      @metadata = {}
       @files = {}
       @client = Client.new
     end
@@ -21,6 +22,40 @@ module AppleNews
 
     def add_string_as_file(name, contents)
       @files[name] = StringIO.new(contents)
+    end
+
+    def is_sponsored?
+      @metadata['isSponsored']
+    end
+    alias_method :sponsored?, :is_sponsored?
+
+    def is_sponsored=(val)
+      @metadata['isSponsored'] = val
+    end
+
+    def is_preview?
+      @metadata['isPreview']
+    end
+    alias_method :preview?, :is_preview?
+
+    def is_preview=(val)
+      @metadata['isPreview'] = val
+    end
+
+    def accessory_text
+      @metadata['accessoryText']
+    end
+
+    def accessory_text=(val)
+      @metadata['accessoryText'] = val
+    end
+
+    def links
+      @metadata['links']
+    end
+
+    def links=(val)
+      @metadata['links'] = val
     end
   end
 end
