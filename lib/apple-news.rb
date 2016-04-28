cb184c73d272e8488a08e9337c17dafb6ceb8fee
Ryan LeFevre <meltingice8917@gmail.com>
2016-04-28 14:27:04 -0400

    Move multipart-post patch to separate file

--- lib/apple-news.rb
+++ lib/apple-news.rb
@@ -6,6 +6,7 @@ require "active_support/core_ext/string/inflections"
 require "base64"
 require "json"
 require 'net/http/post/multipart'
+require 'apple-news/multipart_post_patch'
 require "mime/types"
 
 require "apple-news/version"
@@ -36,31 +37,5 @@ module AppleNews
 
   def config
     @config ||= Configuration.new
-  end
-end
-
-module Parts
-  class FilePart
-    def build_head(boundary, name, filename, type, content_len, opts = {}, headers = {})
-      trans_encoding = opts["Content-Transfer-Encoding"] || "binary"
-      content_disposition = opts["Content-Disposition"] || "form-data"
-
-      part = ''
-      part << "--#{boundary}\r\n"
-      part << "Content-Disposition: #{content_disposition}; name=#{name}; filename=\"#{filename}\"; size=#{content_len}\r\n"
-      part << "Content-Length: #{content_len}\r\n"
-      if content_id = opts["Content-ID"]
-        part << "Content-ID: #{content_id}\r\n"
-      end
-
-      if headers["Content-Type"] != nil
-        part <<  "Content-Type: " + headers["Content-Type"] + "\r\n"
-      else
-        part << "Content-Type: #{type}\r\n"
-      end
-
-      part << "Content-Transfer-Encoding: #{trans_encoding}\r\n"
-      part << "\r\n"
-    end
   end
 end
