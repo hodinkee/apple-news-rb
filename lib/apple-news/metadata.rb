e72af621c2a7825b3fd6981a714d090a120e1d2a
Skip Baney <twelvelabs@gmail.com>
2017-08-29 21:15:20 -0500

    Add missing video_url property to Metadata

--- lib/apple-news/metadata.rb
+++ lib/apple-news/metadata.rb
@@ -3,7 +3,7 @@ module AppleNews
     include Properties
     optional_properties :authors, :campaign_data, :canonical_url, :date_created,
                         :date_modified, :date_published, :excerpt, :generator_identifier,
-                        :keywords, :thumbnail_url
+                        :keywords, :thumbnail_url, :video_url
 
     optional_property :generator_name, "apple-news-rb"
     optional_property :generator_version, AppleNews::VERSION
@@ -12,5 +12,6 @@ module AppleNews
 
     property_inflection :canonical_url, 'canonicalURL'
     property_inflection :thumbnail_url, 'thumbnailURL'
+    property_inflection :video_url, 'videoURL'
   end
 end
